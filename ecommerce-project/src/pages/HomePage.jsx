52c73ac78aea55a65cbaf5c3fa73a86d584d89d7
Tu Le <tuan79194@gmail.com>
2026-02-26 23:06:31 +0700

    063230 Fetch data from the backend-1106PM-26022026

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,14 @@ import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        }).then((data) => {    
+            console.log(data);
+        });
+
     return (
         <>
             <title>Ecommerce Project</title>
