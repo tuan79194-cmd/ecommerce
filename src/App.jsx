3eb391380fd753acca5659a4f0bb73a753c77a25
Tu Le <tuan79194@gmail.com>
2026-02-26 10:27:17 +0700

    6h-Add tracking page to React-1027AM-26022026

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { Checkoutpage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css'
 
 function App() {
@@ -20,6 +21,10 @@ function App() {
         <Route 
             path="orders" 
             element={<OrdersPage />} />
+
+        <Route 
+            path="tracking" 
+            element={<TrackingPage />} />
     </Routes>
   )
 }
