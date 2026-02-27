5587b34edb460d3c4db23f9cbb09358d87af26fd
Tu Le <tuan79194@gmail.com>
2026-02-27 09:21:47 +0700

    064638-get cart from the backend and display quantity-0921AM-27022026

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -8,7 +8,12 @@ import CartIcon from '../assets/images/icons/cart-icon.png';
 
 import './Header.css';
 
-export function Header() {
+export function Header({ cart }) {
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
+
     return (
         <>
             <div className="header">
@@ -38,7 +43,7 @@ export function Header() {
 
                     <NavLink className="cart-link header-link" to="/checkout">
                         <img className="cart-icon" src={ CartIcon } />
-                        <div className="cart-quantity">3</div>
+                        <div className="cart-quantity">{totalQuantity}</div>
                         <div className="cart-text">Cart</div>
                     </NavLink>
                 </div>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -15,13 +15,20 @@ export function HomePage() {
         });
     */
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
                 console.log(response.data);
                 setProducts(response.data);
-        });        
+        });
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                console.log(response.data);
+                setCart(response.data);
+            });
     }, []);
 
     return (
@@ -29,7 +36,7 @@ export function HomePage() {
             <title>Ecommerce Project</title>
             <link rel="icon" type="image/svg+xml" href="/images/home-favicon.png" />
 
-            <Header />
+            <Header cart={cart}/>
 
             <div className="home-page">
                 <div className="products-grid">
