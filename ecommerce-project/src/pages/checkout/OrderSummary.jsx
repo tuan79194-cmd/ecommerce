27d6f6746b44dd2ba11774dbf0ce131483d70d8d
Tu Le <tuan79194@gmail.com>
2026-02-28 16:35:25 +0700

    7b-Create a new component named CartItemDetails-0435PM-28022026

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
-import { formatMoney } from '../../utils/money';
+// import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
+import { CartItemDetails } from './CartItemDetails';
+
 
 export function OrderSummary({ cart, deliveryOptions }) {
     return(
@@ -22,35 +24,8 @@ export function OrderSummary({ cart, deliveryOptions }) {
                             </div>
 
                             <div className="cart-item-details-grid">
-                                <img className="product-image"
-                                    src={cartItem.product.image} />
-
-                                <div className="cart-item-details">
-                                    <div className="product-name">
-                                        {
-                                            cartItem.product.name
-                                        }
-                                    </div>
-                                    <div className="product-price">
-                                        {
-                                            formatMoney(cartItem.product.priceCents)
-                                        }
-                                    </div>
-                                    <div className="product-quantity">
-                                        <span>
-                                            Quantity: 
-                                                <span className="quantity-label">
-                                                {cartItem.quantity}
-                                                </span>
-                                        </span>
-                                        <span className="update-quantity-link link-primary">
-                                            Update
-                                        </span>
-                                        <span className="delete-quantity-link link-primary">
-                                            Delete
-                                        </span>
-                                    </div>
-                                </div>
+                                <CartItemDetails 
+                                    cartItem={cartItem} />
                                 <DeliveryOptions 
                                     cartItem={cartItem} 
                                     deliveryOptions={deliveryOptions} />
