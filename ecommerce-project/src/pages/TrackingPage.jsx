820934abe0283f0a97d2f76b838dfa9ab7ea42f1
Tu Le <tuan79194@gmail.com>
2026-02-28 22:57:36 +0700

    7k-load the data for the Tracking Page from the backend. Using axios, useEffect, and the orderId from the URL-1054PM-28022026(exactly)

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,9 +1,38 @@
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
 import { Link, useParams } from "react-router";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
+
 export function TrackingPage({ cart }) {
     const params = useParams();
     console.log(params);
+<<<<<<< HEAD
+=======
+    */
+    const { orderId, productId } = useParams();
+
+    const [order, setOrder]= useState(null);
+
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        };
+        fetchTrackingData();
+        
+    }, [orderId]);
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    })
+
+>>>>>>> 9987d48 (7k-load the data for the Tracking Page from the backend. Using axios, useEffect, and the orderId from the URL-1054PM-28022026(exactly))
     return (
         <>
             <title>Tracking</title>
@@ -19,18 +48,18 @@ export function TrackingPage({ cart }) {
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
