1218f18a356b6406b8cb3e6a543ab282ab9c49bb
Tu Le <tuan79194@gmail.com>
2026-02-28 21:03:40 +0700

    7h-tracking page added prop into <Header />-0903PM-28022026

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
 
         <Route 
             path="tracking" 
-            element={<TrackingPage />} />
+            element={<TrackingPage cart={cart}/>} />
         
         <Route 
             path="*"
--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,14 +1,14 @@
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
 import { Link } from "react-router";
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
     return (
         <>
             <title>Tracking</title>
 
             <link rel="icon" type="image/svg+xml" href="/images/tracking-favicon.png"/>
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="tracking-page">
                 <div className="order-tracking">
