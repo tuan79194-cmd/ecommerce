802ae9c17430e60d986dfff3c5e18531c4565d1b
Tu Le <tuan79194@gmail.com>
2026-02-28 16:50:06 +0700

    7d-turn back-0449PM-28022026

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -7,7 +7,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <StrictMode>
+                <App />
+            </StrictMode>
         </BrowserRouter>
     </StrictMode>,
 )
