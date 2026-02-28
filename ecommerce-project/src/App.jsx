b3bbe41eec175e891ba4e74e195251671551ad60
Tu Le <tuan79194@gmail.com>
2026-02-28 10:48:56 +0700

    073711-Generate the HTML for the orders page-1048PM-28022026

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
         <Route 
             path="orders" 
-            element={<OrdersPage />} />
+            element={<OrdersPage cart={cart}/>} />
 
         <Route 
             path="tracking" 
