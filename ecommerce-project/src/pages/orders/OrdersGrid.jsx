82ae04eb6353b565d55a29e50295dac9336dc9ce
Tu Le <tuan79194@gmail.com>
2026-03-04 23:05:45 +0700

    Adjust OrdersGrid.jsx with header-1105PM

--- ecommerce-project/src/pages/orders/OrdersGrid.jsx
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -12,39 +12,11 @@ export function OrderGrid({ orders }) {
                 orders.map((order) => {
                     return (
                         <div key={order.id} className="order-container">
-
-                            <div className="order-header">
-                                <div className="order-header-left-section">
-                                    <div className="order-date">
-                                        <div className="order-header-label">Order Placed:</div>
-                                        <div>
-                                            {
-                                                dayjs(order.orderTimeMs)
-                                                .format('MMMM D')
-                                            }
-                                        </div>
-                                    </div>
-                                    <div className="order-total">
-                                        <div className="order-header-label">Total:</div>
-                                        <div>
-                                            {
-                                                formatMoney(order.totalCostCents)
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="order-header-right-section">
-                                    <div className="order-header-label">Order ID:</div>
-                                    <div>
-                                            {order.id}
-                                    </div>
-                                </div>
-                            </div>
-                            <OrderDetailsGrids 
-                                order={order}/>
                             <OrderHeader 
                                 order={order}/>
+                            <OrderDetailsGrids 
+                                order={order}/>
+
                         </div>                                     
                     );
                 
