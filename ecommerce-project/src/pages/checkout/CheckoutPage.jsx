8d9ee42aaf92ade2cff6cba595a9991df804898f
Tu Le <tuan79194@gmail.com>
2026-03-05 22:26:39 +0700

    8b-In CheckoutPage.jsx, seperate the code that reloads the PaymentSummary into another useEffect (this should use the dependency array [cart])-1026PM-05032026

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -25,6 +25,7 @@ export function Checkoutpage({ cart, loadCart }) {
             });
     }, []);
     */
+    /*
     useEffect(() => {
         const fetchCheckoutData = async() => {
             let response = await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
@@ -36,6 +37,32 @@ export function Checkoutpage({ cart, loadCart }) {
 
         fetchCheckoutData();
     }, [cart]);
+    */
+    /*
+    Problem with old useEffect : 
+    Whenever the cart has changed (add/delete items), React must call both APIs.
+    However, deliveryOptions (multiple options : Fast delivery, save delivery) are always 
+    static data, it doesn't change based on how many items you buy
+
+    Solution : Seperate fetching deliveryOptions task into an useEffect ran only once
+    ([]), and also, seperate fetching paymentSummary into another useEffect ran after the cart 
+    has changed ([cart]) 
+
+    */
+    // First useEffect : Only run once whenever the page has loaded (to choose delivery options )
+    useEffect(() => {
+        const fetchDeliveryOptions = async() => {
+            let response = await axios.get('api/delivery-options?expand=estimatedDeliveryTime');
+            setDeliveryOptions(response.data);
+        };
+    }, []);
+    // Second useEffect : original useEffect before being seperated
+    useEffect(() => {
+        const fetchPaymentSummmary = async() => {
+            let response = await axios.get('/api/payment-summary');
+            setPaymentSummary(response.data);
+        }
+    }), [cart];
     return (
         <>
             <title>Checkout</title>
