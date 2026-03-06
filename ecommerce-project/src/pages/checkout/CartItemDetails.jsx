48d3f05f6f5f5895921fb04e9162143f13866236
Tu Le <tuan79194@gmail.com>
2026-03-06 10:22:17 +0700

    8f-Add a state to track if the quantity is being updated-1022AM-06032026

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,10 +1,17 @@
-import { formatMoney  } from "../../utils/money";
+import { formatMoney } from "../../utils/money";
 import axios from 'axios';
+import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+    const [isUdpatingQuantity, setIsUdpatingQuantity] = useState(false);
+
     const deleteCartItem = async() => {
         await axios.delete(`/api/cart-items/${cartItem.productId}`);
         await loadCart();
+    };
+
+    const updateQuantity = () => {
+        setIsUdpatingQuantity(isUdpatingQuantity => !isUdpatingQuantity);
     };
     return(
         <>
@@ -25,14 +32,20 @@ export function CartItemDetails({ cartItem, loadCart }) {
                 <div className="product-quantity">
                     <span>
                         Quantity: 
+                        {
+                            isUdpatingQuantity ? 
                             <input 
                                 type="text" 
-                                className="inputQuantity"/>
+                                className="inputQuantity"
+                            />
+                            : 
                             <span className="quantity-label">
-                            {cartItem.quantity}
+                                {cartItem.quantity}
                             </span>
+                        }
                     </span>
-                    <span className="update-quantity-link link-primary">
+                    <span className="update-quantity-link link-primary"
+                        onClick={updateQuantity}>
                         Update
                     </span>
                     <span className="delete-quantity-link link-primary"
