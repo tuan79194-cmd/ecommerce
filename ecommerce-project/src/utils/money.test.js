3d8d56b167a1aca4ae37291dd7a3e06042fdfe42
Tu Le <tuan79194@gmail.com>
2026-03-13 16:35:56 +0700

    9a-Add a test for formatMoney and check if it works with the number 0 (the result should be '$0.00').-0435PM-13032026

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -9,5 +9,8 @@ describe('formatMoney', () => {
     it('displays 2 decimals', () => {
         expect(formatMoney(1090)).toBe('$10.90');
         expect(formatMoney(100)).toBe('$1.00');
-    });    
+    });
+    it('works with the number 0', () => {
+        expect(formatMoney(0).toBe('$0.00'));
+    });
 });
