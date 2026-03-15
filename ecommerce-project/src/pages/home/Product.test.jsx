52480f6633fe0f6a1d1479e8de0793c064390e55
Tu Le <tuan79194@gmail.com>
2026-03-15 23:00:24 +0700

    9d-userEvent.setup(), user has another method called user.selectOptions(element, value)-1100PM-15032026

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -71,14 +71,27 @@ describe('Product component', () => {
         expect(loadCart).toHaveBeenCalled();    
     });
 
-    it("select a quantity", () => {
+    it("select a quantity", async () => {
         render(<Product 
             product={product} 
             loadCart={loadCart}/>);
-
+        
+        const user = userEvent.setup();
         const quantitySelector = screen.getByTestId("product-quantity-selector");
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        
         expect(quantitySelector).toHaveValue("1");
-        expect(quantitySelector).toHaveValue("2");
-    });
 
+        await user.selectOptions(quantitySelector, '3');
+        await user.click(addToCartButton);
+        expect(quantitySelector).toHaveValue("3");
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 3
+            }
+        );
+        expect(loadCart).toHaveBeenCalled();
+    });
 });
